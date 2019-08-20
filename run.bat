44fb4455a9c5e9afab4ea9cb9857770128a6f8a2
Dev Paudel <dev.paudel@ufl.edu>
2019-08-20 14:41:18 -0400

    add waiting time for chrome to load up
    this helps in opening up the port

--- run.bat
+++ run.bat
@@ -1,3 +1,4 @@
 start "" /MIN "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --window-position=0,0 --window-size=1,1
 SET ROPTS=--no-save --no-environ --no-init-file --no-restore --no-Rconsole
+SLEEP 5
 R-Portable\App\R-Portable\bin\Rscript.exe %ROPTS% runShinyApp.R 1> ShinyApp.log 2>&1
